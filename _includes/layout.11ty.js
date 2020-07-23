78de4599127376b94d1ee5d7635b96e870b56e93
Jenaro Calvino <jen.calvineo@gmail.com>
2020-07-22 22:36:25 -0300

    One more time change to absolute paths

--- _includes/layout.11ty.js
+++ _includes/layout.11ty.js
@@ -5,7 +5,7 @@ module.exports = ({ content }) => /*html*/`
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>My 11ty Site</title>
-        <link rel="stylesheet" href="css/global.css" />
+        <link rel="stylesheet" href="/css/global.css" />
     </head>
         <body>
             <main>
--- _includes/post.11ty.js
+++ _includes/post.11ty.js
@@ -19,7 +19,7 @@ module.exports = (data) => {
         <meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>${title} - My Blog</title>
-        <link rel="stylesheet" href="css/global.css" />
+        <link rel="stylesheet" href="/css/global.css" />
     </head>
         <body class="post">
             <header>
